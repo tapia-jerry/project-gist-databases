c823067563576f3f4ab2dabcc0fc7e94114d080e
tapia-jerry <jerrytapia2@gmail.com>
2019-09-12 03:58:11 +0000

    all tests passed gists database

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -1,4 +1,27 @@
 import requests
+import json
 
 def import_gists_to_database(db, username, commit=True):
-    pass
+    response = requests.get("https://api.github.com/users/{USERNAME}/gists".format(USERNAME=username))
+    response.raise_for_status()
+    gists_data = response.json()
+    
+    for gist in gists_data:
+        gists_dict = {
+            "github_id": gist['id'],
+            "html_url": gist['html_url'],
+            "git_pull_url": gist['git_pull_url'],
+            "git_push_url": gist['git_push_url'],
+            "commits_url": gist['commits_url'],
+            "forks_url": gist['forks_url'],
+            "public": gist['public'],
+            "created_at": gist['created_at'],
+            "updated_at": gist['updated_at'],
+            "comments": gist['comments'],
+            "comments_url": gist['comments_url']}
+        db.execute("""
+            INSERT INTO gists ("github_id", "html_url", "git_pull_url", "git_push_url", "commits_url", "forks_url", "public", "created_at", "updated_at", "comments", "comments_url") 
+            VALUES (:github_id,:html_url,:git_pull_url,:git_push_url,:commits_url,:forks_url,:public,:created_at,:updated_at,:comments,:comments_url)
+        """,gists_dict)
+    if commit:
+        db.commit()
--- gists_database/search.py
+++ gists_database/search.py
@@ -1,4 +1,16 @@
 from .models import Gist
+from datetime import datetime
 
-def search_gists(db_connection, **kwargs):
-    pass
+def search_gists(db_connection, github_id=None, created_at=None):
+    query = None
+    if github_id:
+        query = db_connection.execute('SELECT * FROM gists where github_id = ?', [github_id])
+    elif created_at:
+        query = db_connection.execute('SELECT * FROM gists where datetime(created_at) = datetime(?)', [created_at])
+    else:
+        query = db_connection.execute('SELECT * FROM gists')
+
+    result = []
+    for gist in query.fetchall():
+        result.append(Gist(gist))
+    return result
